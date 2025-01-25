cbcde26a4a963830f2166cbaba884d0335e14d04
Chad Weimer <git@cwmr.dev>
2025-01-25 13:00:37 -0800

    Add wineandcats backup and restore scripts

---
+++ backup-wineandcats-db.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Usage: backup-wineandcats-db.sh <stage> <password> <pgversion>
+#
+# Example: backup-wineandcats-db.sh dev thepassword 16
+
+GOMP_STAGE=$1
+PGPASSWORD=$2
+PGVERSION=$3
+docker stop wineandcats-$GOMP_STAGE
+docker run -it --rm --network wineandcats-$GOMP_STAGE -v $PWD:/backup -e PGHOST=db -e POSTGRES_DB=gomp -e PGPASSWORD=$PGPASSWORD -e PGUSER=gomp postgres:$PGVERSION /bin/sh -c 'pg_dump --schema=public $POSTGRES_DB > /backup/dump.sql'
+docker start wineandcats-$GOMP_STAGE
