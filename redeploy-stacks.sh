182ceca79929b0c2f4e74dcaafafb74d0c5a53f1
Chad Weimer <git@cwmr.dev>
2025-02-02 15:25:35 -0800

    Preserve order of hooks from source file

--- redeploy-stacks.sh
+++ redeploy-stacks.sh
@@ -8,29 +8,29 @@
 #
 # stack1=https://hook.url
 # stack2=https://hook.url
-#
 
-declare -A hooks
+declare -a hooks
 while IFS='=' read -r key value; do
   # Skip lines starting with sharp
   # or lines containing only space or empty lines
   [[ "$key" =~ ^([[:space:]]*|[[:space:]]*#.*)$ ]] && continue
 
-  hooks[$key]=$value
+  # Intentionally not using associative array to preserve order
+  hooks+=("stackname='$key';hookurl='$value'")
 done < "$1"
 
-for key in "${!hooks[@]}"
+for hook in "${hooks[@]}"
 do
-
-  echo "Updating stack '$key'..."
-  curl -sSX POST ${hooks[$key]}
+  eval $hook
+  echo "Updating stack '$stackname'..."
+  curl -sSX POST $hookurl
 
   # Don't try to update all the stacks at once.
   # We're not trying to guarantee each one is complete; just rate limit.
   echo "Sleeping for 1 minute..."
   sleep 1m
 
-  echo "Updated stack '$key'."
+  echo "Updated stack '$stackname'."
 done
 
 docker image prune -f
